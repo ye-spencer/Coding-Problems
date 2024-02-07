6d234788d18536e9d67e7db0407124df5a3778f3
Spencer Ye <57966319+ye-spencer@users.noreply.github.com>
2024-02-07 14:27:40 -0500

    Create reverse_bits.cpp

---
+++ reverse_bits.cpp
@@ -0,0 +1,15 @@
+class Solution 
+{
+public:
+    uint32_t reverseBits(uint32_t n) 
+    {
+        uint32_t result = 0;
+        for (size_t i = 0; i < 32; i++)
+        {
+            result <<= 1;
+            result += (n & 1U);
+            n >>= 1;
+        }
+        return result;
+    }
+};
